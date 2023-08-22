6c13242e083088f60cb8769d3613b719f3b33e8c
Kevin Huayllas <KevinOmega01@gmail.com>
2023-08-22 11:57:40 -0400

    adding rect green colors

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 import "./canvas.css";
+import * as topojson from "topojson";
 
 const Canvas = () => {
   const [us, setUs] = useState({});
@@ -22,12 +23,50 @@ const Canvas = () => {
 
   useEffect(() => {
     if (us !== {} && educationalData.length) {
+      let minValue = 2.56;
+      let maxValue = 75.1;
+      let steps = 8;
+
+      const color = d3
+        .scaleThreshold()
+        .domain(d3.range(minValue, maxValue, (maxValue - minValue) / steps))
+        .range(d3.schemeGreens[9]);
+
+      const path = d3.geoPath();
+
+      const x = d3.scaleLinear().domain([minValue, maxValue]).range([600, 806]);
+
       const svg = d3
         .select("#canvas-container")
         .append("svg")
         .attr("id", "svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
+        .attr("width", "70%")
+        .attr("height", "100%");
+
+      const g = svg
+        .append("g")
+        .attr("id", "legend")
+        .attr("transform", "translate(0, 30)");
+
+      g.selectAll("rect")
+        .data(
+          color.range().map((d) => {
+            d = color.invertExtent(d);
+            if (d[0] === null) {
+              d[0] = x.domain()[0];
+            }
+            if (d[1] === null) {
+              d[1] = x.domain()[1];
+            }
+            return d;
+          })
+        )
+        .enter()
+        .append("rect")
+        .attr("fill", (d) => color(d[0]))
+        .attr("x", (d) => x(d[0]))
+        .attr("width", (d) => (d[0] && d[1] ? x(d[1]) - x([d[0]]) : x(null)))
+        .attr("height", 8);
     }
   }, [us, educationalData]);
 
